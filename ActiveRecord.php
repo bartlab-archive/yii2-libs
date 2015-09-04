c976ab80b6ebeba3e76c3e358d30d9152f002adf
B@RT <bartlab@gmail.com>
2015-09-04 16:10:17 +0300

    add phpdoc

--- ActiveRecord.php
+++ ActiveRecord.php
@@ -29,6 +29,10 @@ class ActiveRecord extends \yii\db\ActiveRecord {
 		}
 	}
 
+	/**
+	 * @inheritdoc
+	 * @return ActiveQuery the newly created [[ActiveQuery]] instance.
+	 */
 	public static function find() {
 		return Yii::createObject(ActiveQuery::className(), [get_called_class()]);
 	}
