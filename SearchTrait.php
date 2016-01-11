83031b25b8481ebfe38c2a09be1a2739f649f08d
B@RT <bartlab@gmail.com>
2016-01-11 15:57:40 +0200

    Update SearchTrait.php

--- SearchTrait.php
+++ SearchTrait.php
@@ -96,15 +96,10 @@ trait SearchTrait {
 
 	/**
 	 * Получение DataProvider
-	 *
+	 * 
 	 * @param array $options [опционально] опции для DataProvider
 	 *
 	 * @return \yii\data\ActiveDataProvider
-	 */
-	/**
-	 * @param array $options
-	 *
-	 * @return object
 	 * @throws InvalidConfigException
 	 */
 	public function dataProvider($options = []) {
